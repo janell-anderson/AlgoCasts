d157c31f786260a7d3f5eb71f33c6ba8721b9b30
Janell Anderson <andersonjanell@gmail.com>
2019-03-07 20:18:33 -0500

    Learned two more possible solutions and learned about debugger

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,52 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split("").reverse().join("");
-}
+  debugger;
+  return str.split('').reduce((rev,char) => char + rev, '');
 
+  reverse('asdf');
+
+  }
 module.exports = reverse;
+// My solution
+
+// return str.split('').reverse().join('');
+
+// My version of the for loop
+
+//   let newStr = '';
+//
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     newStr += str[i];
+//   }
+//
+//   return newStr;
+
+//Stephen's 1st solution
+
+// const arr = str.split('');
+// arr.reverse();
+// return arr.join('');
+
+
+// Stephen's 2nd solution using a for loop
+// for each character of string
+// character is temporary variable that is redelcared
+
+// let reversed = '';
+// for (let character of str) {
+//   reversed = character + reversed;
+// }
+// return reversed;
+
+// Stephen's 3rd solution using reduce()
+
+// reduce() method executes a reducer function (that you provide) on each member of the array resulting in a single output value.
+// ES2015
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+//
+//   //ES6
+//   return str.split('').reduce((rev,char) => char + rev, '');
+// }
